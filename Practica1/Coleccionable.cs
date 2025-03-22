cb1a85b15650289cb44b895f0fab7c67f617e9ad
niasua <niasua@Nico-PC.>
2025-03-22 21:04:36 +0000

    Se implemento interface Coleccionable

--- Practica1/Coleccionable.cs
+++ Practica1/Coleccionable.cs
@@ -2,5 +2,12 @@ using System;
 
 namespace Practica1
 {
-    
+    // Se implementó interface Coleccionable (Ejercicio 3)
+    public interface Coleccionable {
+        int cuantos();
+        Comparable minimo();
+        Comparable maximo();
+        void agregar(Comparable c);
+        bool contiene (Comparable c);
+    }
 }
