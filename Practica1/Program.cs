d3f474112c01175e2c23519be0abf067f73b9be8
niasua <niasua@Nico-PC.>
2025-03-23 00:51:31 +0000

    Instancias de Pila y Cola creadas, ejecutando funciones de llenar e informar

--- Practica1/Program.cs
+++ Practica1/Program.cs
@@ -4,14 +4,20 @@ namespace Practica1
 {
     public class Program {
         static void Main(string[] args) {
-        
+            Pila pila1 = new Pila();
+            Cola cola1 = new Cola();
+            llenar(pila1);
+            llenar(cola1);
+            informar(pila1);
+            informar(cola1);
         }
 
         // Ejercicio 6
-        public void informar(Coleccionable coleccion) {
+        public static void informar(Coleccionable coleccion) {
             Console.WriteLine(coleccion.cuantos());
             Console.WriteLine(coleccion.minimo());
             Console.WriteLine(coleccion.maximo());
+            Console.WriteLine("Ingrese un número:");
             Comparable c = new Numero(int.Parse(Console.ReadLine()));
             if (coleccion.contiene(c))
                 Console.WriteLine("El elemento leído está en la colección");
@@ -20,7 +26,7 @@ namespace Practica1
         }
 
         // Ejercicio 5
-        public void llenar(Coleccionable c) {
+        public static void llenar(Coleccionable c) {
             for (int i = 0; i < 20; i++) {
                 Comparable nuevoComparable = new Numero(i);
                 c.agregar(nuevoComparable);
