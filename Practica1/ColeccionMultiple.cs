a9752e0df37c3569c7ae1e12d7c0423b0f3373a7
niasua <niasua@Nico-PC.>
2025-03-23 15:26:19 +0000

    Se creo la clase ColeccionMultiple

---
+++ Practica1/ColeccionMultiple.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Practica1 {
+    // (Ejercicio 8)
+    public class ColeccionMultiple : Coleccionable {
+        // atributos
+        private Pila pila;
+        private Cola cola;
+
+        // constructor
+        public ColeccionMultiple(Pila pila, Cola cola) {
+            this.pila = pila;
+            this.cola = cola;
+        }
+
+        // Implementaciones de Coleccionable
+        public int cuantos() {
+            return pila.cuantos() + cola.cuantos();
+        }
+        public Comparable minimo() {
+            // crea un auxiliar con el minimo de la pila
+            Comparable aux = pila.minimo();
+            // compara el minimo de la cola con el auxiliar para reemplazar o no
+            if (cola.minimo().sosMenor(aux)) {
+                aux = cola.minimo();
+            }
+            return aux;
+        }
+        public Comparable maximo() {
+            Comparable aux = pila.maximo();
+            if (cola.maximo().sosMayor(aux)) {
+                aux = cola.maximo();
+            }
+            return aux;
+        }
+
+        // agregar no hace nada
+        public void agregar(Comparable c) {}
+
+
+        public bool contiene (Comparable c) {
+            return pila.contiene(c) || cola.contiene(c);
+        }
+    }
+}
--- Practica1/obj/Debug/net8.0/Practica1.AssemblyInfo.cs
+++ Practica1/obj/Debug/net8.0/Practica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f474112c01175e2c23519be0abf067f73b9be8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1b481fe5de55619644e0f54e499dd0bbc7ae0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
