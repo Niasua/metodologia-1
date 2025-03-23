b2e8e28909b17f8bb6edcfde2d79910f47813c5b
niasua <niasua@Nico-PC.>
2025-03-23 00:19:55 +0000

    Funcion informar implementada

--- Practica1/Program.cs
+++ Practica1/Program.cs
@@ -4,17 +4,27 @@ namespace Practica1
 {
     public class Program {
         static void Main(string[] args) {
-
+        
         }
 
+        // Ejercicio 6
+        public void informar(Coleccionable coleccion) {
+            Console.WriteLine(coleccion.cuantos());
+            Console.WriteLine(coleccion.minimo());
+            Console.WriteLine(coleccion.maximo());
+            Comparable c = new Numero(int.Parse(Console.ReadLine()));
+            if (coleccion.contiene(c))
+                Console.WriteLine("El elemento leído está en la colección");
+            else
+                Console.WriteLine("El elemento leído no está en la colección");
+        }
+
+        // Ejercicio 5
         public void llenar(Coleccionable c) {
             for (int i = 0; i < 20; i++) {
                 Comparable nuevoComparable = new Numero(i);
                 c.agregar(nuevoComparable);
             }
         }
-        
     }
-
-
 }
