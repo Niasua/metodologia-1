d0307dd4e68ee881a08ea9650996e8274bc379dc
Niasua <nsuarez2410@gmail.com>
2025-04-13 19:52:34 -0300

    teacher teaching a class, se creo una nueva fabrica de alumnos

--- Practica4/Program.cs
+++ Practica4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MetodologíasDeProgramaciónI;
+using Practica4;
+
+namespace Practica3 {
+    public class Program {
+        static void Main(string[] args) {
+            // Ejercicio 4 Practica 4
+            Teacher teacher = new Teacher();
+            for(int i = 0; i < 20; i++) {
+                if(i < 10){
+                    Alumno alumno = (Alumno) new FabricaDeAlumnos().crearAleatorio();
+                    Student student = new AlumnoAdapter(alumno);
+                    teacher.goToClass(student);
+                } else {
+                    AlumnoMuyEstudioso alumno = (AlumnoMuyEstudioso) new FabricaDeAlumnosMuyEstudiosos().crearAleatorio();
+                    Student student = new AlumnoAdapter(alumno);
+                    teacher.goToClass(student);
+                }
+            }
+            teacher.teachingAClass();
+        }
+
+
+        
+    }
+}
