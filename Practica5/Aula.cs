1e8076106352b39956041d3591207763b7480737
Niasua <nsuarez2410@gmail.com>
2025-04-26 16:45:14 -0300

    Se creo Aula

---
+++ Practica5/Aula.cs
@@ -0,0 +1,22 @@
+using System;
+using MetodologíasDeProgramaciónI;
+
+namespace Practica5 {
+    public class Aula {
+        private Teacher teacher;
+
+        public void comenzar() {
+            Console.WriteLine("Comienza la clase...");
+            teacher = new Teacher();
+        }
+
+        public void nuevoAlumno(Alumno alumno) {
+            this.teacher.goToClass(new AlumnoAdapter(alumno));
+        }
+
+        public void claseLista() {
+            this.teacher.teachingAClass();
+        }
+
+    }
+}
