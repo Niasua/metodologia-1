1156ea68d9a37709e2e510499ee4c037aba8c68c
Niasua <nsuarez2410@gmail.com>
2025-04-26 17:20:22 -0300

    Se creo Ordenable

---
+++ Practica5/Ordenable.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Practica5 {
+    public interface Ordenable {
+        void setOrdenInicio(OrdenEnAula1 orden);
+        void setOrdenLlegaAlumno(OrdenEnAula2 orden);
+        void setOrdenAulaLlena(OrdenEnAula1 orden);
+    }
+}   
--- Practica5/obj/Debug/net8.0/Practica5.AssemblyInfo.cs
+++ Practica5/obj/Debug/net8.0/Practica5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4626268f0c056b52eddba174e1c5e8d4eb6b050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ce336934e89d77145df2247bc7152426ab1b3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
